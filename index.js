c883472d041fa3c921dd84fd03d7053cfa90cd98
mdrafiur <rafiur.rashid@mail.utoronto.ca>
2020-08-27 02:31:48 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const SlackBot = require('slackbots');
+const axios = require('axios')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const bot = new SlackBot({
+    token: `${process.env.BOT_TOKEN}`,
+    name: 'zribbot'
+})
+
+// Start Handler
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':robot_face:'
+    }
+
+    bot.postMessageToChannel(
+        'runtime-image-build-bot',
+        'Get inspired while working with @zribbot',
+        params
+    );
+})
+  
+// Error Handler
+bot.on('error', (err) => {
+    console.log(err);
+})
+
+// Message Handler
+bot.on('message', (data) => {
+    if(data.type !== 'message') {
+        return;
+    }
+    handleMessage(data.text);
+})
+
